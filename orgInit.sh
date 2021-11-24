c25074667a5615866cd0ceb67dc3346ebc686244
PraisyAbraham13 <53878429+PraisyAbraham13@users.noreply.github.com>
2021-11-24 10:07:57 -0800

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,6 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix platform -o events.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix Permission -o sets.demo
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
 
-# Install Streaming Monitor
-sfdx force:package:install --package 04t1t000003DLAQAA4 -w 20
+
