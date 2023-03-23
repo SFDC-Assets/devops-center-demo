fb5a863b8ee99966fdc2c7b8ca29590eac0c46e1
zhou059 <zhou059@gmail.com>
2023-03-23 15:57:38 -0700

    polished

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,15 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix Permission -o sets.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix devops -o center.d1
+sfdx force:package:install -p 04t6g000008nzOu -w 15 -r
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx shane:user:permset:assign -l User -g User -n sf_devops_NamedCredentials
+sfdx shane:user:permset:assign -l User -g User -n sf_devops_InitializeEnvironments
+sfdx shane:user:permset:assign -l User -g User -n DevOps_Center
+sfdx shane:user:permset:assign -l User -g User -n DevOps_CenterManager
+sfdx shane:user:permset:assign -l User -g User -n DevOps_CenterRelease
 sfdx force:org:open
 
+# Create 2nd org
 
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix devops -o center.d2
+sfdx shane:user:password:set -g User -l User -p salesforce1
