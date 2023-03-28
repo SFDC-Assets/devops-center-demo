a8fc3a98b8469b61567b3e68ff4878e8a02fd6f6
zhou059 <zhou059@gmail.com>
2023-03-27 22:30:08 -0700

    adjust definition

--- orgInit.sh
+++ orgInit.sh
@@ -11,5 +11,5 @@ sfdx force:org:open
 
 # Create 2nd org
 
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix devops -o center.d2
-sfdx shane:user:password:set -g User -l User -p salesforce1
+# sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix devops -o center.d2
+# sfdx shane:user:password:set -g User -l User -p salesforce1
